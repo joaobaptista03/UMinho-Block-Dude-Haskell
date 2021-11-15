839004524727952777f637102fa840b7ac57ad17
Tuga <Tuga>
2021-11-15 22:31:10 +0000

    Ponto 3 da Tarefa 1. (Removidos testes pois não faziam mais sentido no contexto)

--- src/Tarefa1_2021li1g006.hs
+++ src/Tarefa1_2021li1g006.hs
@@ -15,30 +15,61 @@ import LI12122
 --Testa se as coordenadas "c" aparecem alguma vez no resto da lista (Mapa) dada. (f1 do Ponto 1)
 elem' :: Coordenadas -> [(Peca, Coordenadas)] -> Bool
 elem' c [] = False
-elem' c ((a1,b1):t) | c == b1 = True
+elem' c ((p,cs):t) | c == cs = True
                     | otherwise = elem' c t
 
 --Testa, usando a função elem', se existe alguma coordenada repetida na lista (Mapa) dada. (f2 do Ponto 1)
 repetido :: [(Peca, Coordenadas)] -> Bool
 repetido [] = False
 repetido [_] = False
-repetido  ((a1,b1):t) | elem' b1 t = True
+repetido  ((p,cs):t) | elem' cs t = True
                       | otherwise = repetido t
 
 --Conta quantas portas existem na lista (Mapa) dada. (f1 do Ponto 2)
 quantasPortas :: [(Peca, Coordenadas)] -> Int
 quantasPortas [] = 0
-quantasPortas [(a1,b1)] | a1 == Porta = 1
-                        | otherwise = 0
-quantasPortas ((a1,b1):t) | a1 == Porta = 1 + quantasPortas t
-                          | otherwise = quantasPortas t
+quantasPortas ((p,cs):t) | p == Porta = 1 + quantasPortas t
+                        | otherwise = quantasPortas t
 
 --Testa, usando a função quantasPortas, se apenas existe 1 porta na lista (Mapa) dada. (f2 do Ponto 2)
 so1Porta :: [(Peca, Coordenadas)] -> Bool
-so1Porta ((a1,b1):t) = quantasPortas ((a1,b1):t) == 1
+so1Porta ((p,cs):t) = quantasPortas ((p,cs):t) == 1
+
+--Dada uma lista (Mapa), produz uma lista com todas as caixas da mesma. (f1 do Ponto 3)
+listaCaixas :: [(Peca, Coordenadas)] -> [(Peca, Coordenadas)]
+listaCaixas [] = []
+listaCaixas ((p,cs):t) | p == Caixa = (p,cs) : listaCaixas t
+                       | otherwise = listaCaixas t
+
+--Dada uma caixa, verifica se essa caixa está em cima de outra caixa ou de um bloco, e se não se encontra a flutuar. (f2 do Ponto 3)
+testaCaixa :: [(Peca, Coordenadas)] -> (Peca, Coordenadas) -> Bool
+testaCaixa [] (Caixa,(x,y)) = False
+testaCaixa ((p,(x2,y2)):t) (Caixa,(x,y)) | (p == Caixa || p == Bloco) && x == x2 && y+1 == y2 = True
+                                         | otherwise = testaCaixa t (Caixa,(x,y))
+
+--Dada uma lista (Mapa), testa todas as caixas usando as funções testaCaixa e listaCaixas, usando "all" para verificar se todas as caixas são válidas. (f3 do Ponto 3)
+verificaCaixas :: [(Peca, Coordenadas)] -> Bool
+verificaCaixas [] = True
+verificaCaixas ((p,cs):t) = let caixas = listaCaixas ((p,cs):t) in
+    all (testaCaixa ((p,cs):t)) (listaCaixas ((p,cs):t))
+
+--Cria uma lista das coordenadas y da lista (Mapa) dada. (f1 do Ponto 4)
+listay :: [(Peca, Coordenadas)] -> [Int]
+listay [] = []
+listay ((p, (x,y)) : t) = y : listay t
+
+--Cria uma lista das coordenadas x da lista (Mapa) dada. (f2 do Ponto 4)
+listax :: [(Peca, Coordenadas)] -> [Int]
+listax [] = []
+listax ((p, (x,y)) : t) = x : listax t
+
+haespaco :: [(Peca, Coordenadas)] -> Bool
+haespaco [] = False
+haespaco ((p,cs):t) = let area = (maximum (listay ((p,cs):t)) + 1) * (maximum (listax ((p,cs):t)) + 1) in
+    length ((p,cs):t) < area
 
 --Testa a validade do mapa pelos pontos 1 a 5 da Tarefa 1.
 validaPotencialMapa :: [(Peca, Coordenadas)] -> Bool
-validaPotencialMapa [] = True
-validaPotencialMapa [(a1,b1)] = True
-validaPotencialMapa ((a1,b1):t) = not (repetido ((a1,b1):t)) && so1Porta ((a1,b1):t)
+validaPotencialMapa [] = False
+validaPotencialMapa [(p,cs)] = True
+validaPotencialMapa l = not (repetido l) && so1Porta l && verificaCaixas l && haespaco l
--- tests/Tarefa1_2021li1g006_Spec.hs
+++ tests/Tarefa1_2021li1g006_Spec.hs
@@ -8,14 +8,18 @@ import Fixtures
 -- Tarefa 1
 testsT1 =
   test
-    ["Tarefa 1 - Teste 1 - Vazio" ~: validaPotencialMapa [] ~=?  True
-    , "Tarefa 1 - Teste 2 - 2 Peças" ~: validaPotencialMapa [(Porta, (0,0)), (Bloco, (0,1))] ~=?  True
+    ["Tarefa 1 - Teste 1 - Vazio" ~: validaPotencialMapa [] ~=?  False
+    , "Tarefa 1 - Teste 2 - 2 Peças" ~: validaPotencialMapa [(Porta, (0,0)), (Porta, (0,1))] ~=?  False
     , "Tarefa 1 - Teste 3" ~: validaPotencialMapa [(Porta, (0,0)), (Bloco, (0,1)), (Bloco, (0,1))] ~=?  False
     , "Tarefa 1 - Teste 4" ~: validaPotencialMapa [(Porta, (0,0)), (Bloco, (0,1)), (Bloco, (0,2)), (Bloco, (0,1))] ~=?  False
     , "Tarefa 1 - Teste 5" ~: validaPotencialMapa [(Porta, (0,0)), (Bloco, (0,1)), (Bloco, (0,2)), (Bloco, (0,3)), (Bloco, (0,1))] ~=?  False
-    , "Tarefa 1 - Teste 6" ~: validaPotencialMapa [(Porta, (0,0)), (Bloco, (0,1)), (Bloco, (0,2)), (Bloco, (0,3)), (Bloco, (0,4))] ~=?  True
     , "Tarefa 1 - Teste 7" ~: validaPotencialMapa m1 ~=?  True
     , "Tarefa 1 - Teste 8 - 2 Portas" ~: validaPotencialMapa [(Porta, (0,0)), (Porta, (0,1))] ~=?  False
     , "Tarefa 1 - Teste 9 - 2 Portas" ~: validaPotencialMapa [(Porta, (0,0)), (Porta, (0,0))] ~=?  False
     , "Tarefa 1 - Teste 10" ~: validaPotencialMapa [(Porta, (0,0)), (Bloco, (0,1)), (Porta, (0,1))] ~=?  False
+    , "Tarefa 1 - Teste 11" ~: validaPotencialMapa [(Porta, (0,0)), (Bloco,(0,5)),(Bloco,(1,5)),(Bloco,(2,5)),(Bloco,(3,5)),(Caixa,(0,4)),(Caixa,(1,4)),(Caixa,(2,4)),(Caixa,(3,4))] ~=?  True
+    , "Tarefa 1 - Teste 12" ~: validaPotencialMapa [(Porta, (0,0)), (Bloco,(0,5)),(Bloco,(1,5)),(Bloco,(2,5)),(Bloco,(3,5)),(Caixa,(0,1)),(Caixa,(1,4)),(Caixa,(2,4)),(Caixa,(3,4))] ~=?  False
+    , "Tarefa 1 - Teste 13" ~: validaPotencialMapa [(Porta, (0,0)), (Bloco,(0,5)),(Bloco,(1,5)),(Bloco,(2,5)),(Bloco,(3,5)),(Caixa,(0,4)),(Caixa,(1,4)),(Caixa,(2,4)),(Caixa,(6,4))] ~=?  False
+    , "Tarefa 1 - Teste 14" ~: validaPotencialMapa [(Porta, (0,0)), (Bloco,(0,5)),(Bloco,(1,5)),(Bloco,(2,5)),(Bloco,(3,5)),(Caixa,(0,4)),(Caixa,(1,4)),(Caixa,(2,5)),(Caixa,(3,4))] ~=?  False
+    , "Tarefa 1 - Teste 15" ~: validaPotencialMapa [(Porta, (0,0)), (Bloco,(0,5)),(Bloco,(1,5)),(Bloco,(2,5)),(Bloco,(3,5)),(Bloco,(0,4)),(Caixa,(1,4)),(Caixa,(2,4)),(Caixa,(3,4)),(Caixa,(0,3))] ~=? True 
     ]
