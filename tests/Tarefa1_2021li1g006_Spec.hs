f9fae6940b647e6c33783a0ad4aaa7ac0e54bcb6
Tuga <Tuga>
2021-11-15 11:37:35 +0000

    Ponto 1 da Tarefa 1.

--- tests/Tarefa1_2021li1g006_Spec.hs
+++ tests/Tarefa1_2021li1g006_Spec.hs
@@ -8,7 +8,11 @@ import Fixtures
 -- Tarefa 1
 testsT1 =
   test
-    [ "Tarefa 1 - Teste Valida Mapa m1r" ~: validaPotencialMapa m1 ~=? True
-    , "Tarefa 1 - Teste Valida Mapa vazio" ~: validaPotencialMapa [] ~=? False
-    , "Tarefa 1 - Teste Valida Mapa com 2 portas" ~: validaPotencialMapa [(Porta, (0,0)), (Porta, (1,0))] ~=?  False
+    ["Tarefa 1 - Teste Valida Ponto1 Vazio" ~: validaPotencialMapa [] ~=?  True
+    , "Tarefa 1 - Teste Valida Ponto1 2 Peças" ~: validaPotencialMapa [(Porta, (0,0)), (Bloco, (0,1))] ~=?  True
+    , "Tarefa 1 - Teste Valida Ponto1 3" ~: validaPotencialMapa [(Porta, (0,0)), (Bloco, (0,1)), (Bloco, (0,1))] ~=?  False
+    , "Tarefa 1 - Teste Valida Ponto1 4" ~: validaPotencialMapa [(Porta, (0,0)), (Bloco, (0,1)), (Bloco, (0,2)), (Bloco, (0,1))] ~=?  False
+    , "Tarefa 1 - Teste Valida Ponto1 5" ~: validaPotencialMapa [(Porta, (0,0)), (Bloco, (0,1)), (Bloco, (0,2)), (Bloco, (0,3)), (Bloco, (0,1))] ~=?  False
+    , "Tarefa 1 - Teste Valida Ponto1 6" ~: validaPotencialMapa [(Porta, (0,0)), (Bloco, (0,1)), (Bloco, (0,2)), (Bloco, (0,3)), (Bloco, (0,4))] ~=?  True
+    , "Tarefa 1 - Teste Valida Ponto1 7" ~: validaPotencialMapa m1 ~=?  True
     ]
