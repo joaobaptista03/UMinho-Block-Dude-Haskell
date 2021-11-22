e295c4f2ffdefa6c849790c0e045ce4b325fd8a3
Tuga <Tuga>
2021-11-22 11:06:16 +0000

    Parte da Tarefa 2 feita.

--- src/Tarefa2_2021li1g006.hs
+++ src/Tarefa2_2021li1g006.hs
@@ -10,8 +10,34 @@ module Tarefa2_2021li1g006 where
 
 import LI12122
 
-constroiMapa :: [(Peca, Coordenadas)] -> Mapa
-constroiMapa pecas = undefined
+-- |Cria uma lista das coordenadas y da lista (Mapa) dada.
 
-desconstroiMapa :: Mapa -> [(Peca, Coordenadas)]
-desconstroiMapa mapa = undefined
+-- |Por exemplo:
+
+-- |listay [(Porta,(0,0)),(Bloco,(1,2)),(Bloco,(2,2)),(Caixa,(2,1))] = [0,2,2,1]
+
+-- |listay [(Porta,(0,0)),(Bloco,(1,2)),(Bloco,(2,2)),(Caixa,(2,0))] = [0,2,2,0]
+listay :: [(Peca, Coordenadas)] -> [Int]
+listay [] = []
+listay ((p, (x,y)) : t) = y : listay t
+
+-- |Cria uma lista da lista (Mapa) dada, sem a coluna c.
+
+-- |Por exemplo:
+
+-- |listaslinha 1 [(Porta,(0,0)),(Bloco,(1,2)),(Bloco,(2,2)),(Caixa,(2,1))] = [(Porta,(0,0)),(Bloco,(1,2)),(Bloco,(2,2))]
+
+-- |listaslinha 2 [(Porta,(0,0)),(Bloco,(1,2)),(Bloco,(2,2)),(Caixa,(2,1))] = [(Porta,(0,0)),(Caixa,(2,1))]
+listaslinha :: Int -> [(Peca, Coordenadas)] -> [(Peca, Coordenadas)]
+listaslinha c [] = []
+listaslinha c ((p, (x,y)) : t) | c == y = listaslinha c t
+                                | otherwise = (p, (x,y)) : (listaslinha c t)
+
+constroiPecas :: Int -> [(Peca, Coordenadas)] -> [Peca]
+constroiPecas y [] = []
+constroiPecas y ((p,(x,y2)):t) = if y == y2 then p : constroiPecas y t else constroiPecas y t
+
+constroiMapa :: [(Peca, Coordenadas)] -> Mapa
+constroiMapa [] = []
+constroiMapa ((p, (x,y)) : t) = let miny = minimum (listay ((p, (x,y)) : t)) in
+    constroiPecas miny ((p, (x,y)) : t) : constroiMapa (listaslinha miny ((p, (x,y)) : t) )
