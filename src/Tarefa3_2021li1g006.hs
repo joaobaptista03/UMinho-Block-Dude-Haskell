92da8818c399c8b687c95e9ba35b2c9ab3bbb4ba
Tuga <Tuga>
2021-11-28 23:55:08 +0000

    Resto da t3

--- src/Tarefa3_2021li1g006.hs
+++ src/Tarefa3_2021li1g006.hs
@@ -13,6 +13,17 @@ import LI12122
 instance Show Jogo where
   show = mostrar
 
+-- |Esta função retorna a string "<" se o jogador estiver virado para a esquerda (Oeste), e ">" em caso contrário.
+direcao1 :: Direcao -> String
+direcao1 d | d == Oeste = "<"
+           | d == Este = ">"
+
+mostrar1 :: Jogo -> String
+mostrar1 (Jogo [] (Jogador (x,y) _ _)) = ""
+mostrar1 (Jogo ([]:t) (Jogador (x,y) d c)) = "\n" ++ mostrar1 (Jogo t (Jogador (x,y-1) d c))
+mostrar1 (Jogo ((p:ps):t) (Jogador (x,y) d c)) | x == 0 && y == 0 = direcao1 d ++ mostrar1 (Jogo (ps:t) (Jogador (-1,-1) d c))
+                                                              | y == 0 = peca1 p ++ mostrar1 (Jogo (ps:t) (Jogador (x-1,y) d c))
+                                                              | otherwise = peca1 p ++ mostrar1 (Jogo (ps:t) (Jogador (x,y) d c))
 
 -- |Esta função retorna a string "X" se a Peça for um Bloco, "C" se for Caixa, "P" se for uma porta, e " " se for Vazio.
 peca1 :: Peca -> String
@@ -31,3 +42,6 @@ auxiliar (h:t) (x,y) | x == 0 && y == 0 = 'C' : t
 caixa :: Coordenadas -> Coordenadas 
 caixa (x,y) = (x,y-1)
 
+mostrar :: Jogo -> String 
+mostrar j@(Jogo _ (Jogador (x,y) _ c)) | c = auxiliar (mostrar1 j) (caixa (x,y))
+                                       | otherwise = mostrar1 j
