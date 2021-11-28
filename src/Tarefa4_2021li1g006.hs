e8f230024e69c8a66c175973f28822aeb8ef6840
Tuga <Tuga>
2021-11-28 21:00:31 +0000

    documentação melhorada (falta exemplos)

--- src/Tarefa4_2021li1g006.hs
+++ src/Tarefa4_2021li1g006.hs
@@ -13,20 +13,38 @@ import GHC.Stack.Types (CallStack(FreezeCallStack))
 import Language.Haskell.TH (javaScript)
 
 
--- Aplica o efeito de um comando sobre o jogador
+-- |Aplica o efeito de um comando sobre o jogador.
+
+-- |Por exemplo:
+
+-- |moveJogador
+
+-- |moveJogador 
 moveJogador :: Jogo -> Movimento -> Jogo
 moveJogador (Jogo m j) AndarDireita = Jogo m (andaDirJogador j m)
 moveJogador (Jogo m j) AndarEsquerda = Jogo m (andaEsqJogador j m)
 moveJogador (Jogo m j) Trepar = Jogo m (trepa j m)
 moveJogador j InterageCaixa = interageCaixa j
 
--- Aplica consecutivamente os comandos dados pela lista
+-- |Aplica consecutivamente os comandos dados pela lista.
+
+-- |Por exemplo:
+
+-- |correrMovimentos 
+
+-- |correrMovimentos 
 correrMovimentos :: Jogo -> [Movimento] -> Jogo
 correrMovimentos jogo [] = jogo
 correrMovimentos jogo (h:t) = correrMovimentos (moveJogador jogo h) t
 
 
--- A função permite ao jogador trepar, se possível, o obstáculo à sua frente
+-- |A função permite ao jogador trepar, se possível, o obstáculo à sua frente.
+
+-- |Por exemplo:
+
+-- |trepa 
+
+-- |trepa  
 trepa :: Jogador -> Mapa -> Jogador 
 trepa j@(Jogador (x,y) dir caixa) m 
                      | not caixa && dir == Este && (getPeca m (x+1) y == Bloco || getPeca m (x+1) y == Caixa) && (getPeca m (x+1) (y+1) == Vazio || getPeca m (x+1) (y+1) == Porta) = Jogador (x+1,y+1) dir caixa
@@ -40,7 +58,13 @@ trepa j@(Jogador (x,y) dir caixa) m
 -- NOTA: confirmar que nao ha peças vazias quando y é 0
 -- NOTA2: adicionar caso quando é porta
 
--- Faz com que o jogador se volte para Este e avance, se for possível, uma unidade
+-- |Faz com que o jogador se volte para Este e avance, se for possível, uma unidade.
+
+-- |Por exemplo:
+
+-- |andaDirJogador 
+
+-- |andaDirJogador 
 andaDirJogador :: Jogador -> Mapa -> Jogador
 andaDirJogador (Jogador (x,y) dir caixa) m 
              | getPeca m (x+1) y == Bloco || getPeca m (x+1) y == Caixa = Jogador (x,y) Este caixa
@@ -48,7 +72,13 @@ andaDirJogador (Jogador (x,y) dir caixa) m
                     if getPeca m (x+1) (y-1) == Bloco || getPeca m (x+1) (y-1) == Caixa then Jogador (x+1,y) Este caixa
                     else andaDirJogador (Jogador (x,y-1) Este caixa) m
 
--- Faz com que o jogador se volte para Oeste e avance, se for possível, uma unidade
+-- |Faz com que o jogador se volte para Oeste e avance, se for possível, uma unidade.
+
+-- |Por exemplo:
+
+-- |andaEsqJogador 
+
+-- |andaEsqJogador  
 andaEsqJogador :: Jogador -> Mapa -> Jogador 
 andaEsqJogador (Jogador (x,y) esq caixa) m 
              | getPeca m (x-1) y == Bloco || getPeca m (x-1) y == Caixa = Jogador (x,y) Oeste caixa
@@ -57,7 +87,13 @@ andaEsqJogador (Jogador (x,y) esq caixa) m
                     else andaEsqJogador (Jogador (x,y-1) Oeste caixa) m
 
 
--- A função permite ao jogador carregar/largar uma caixa
+-- |A função permite ao jogador carregar/largar uma caixa.
+
+-- |Por exemplo:
+
+-- |interageCaixa  
+
+-- |interageCaixa 
 interageCaixa :: Jogo -> Jogo  
 interageCaixa jog@(Jogo m j@(Jogador (x,y) dir caixa)) | caixa && dir == Este && getPeca m (x+1) (y+1) == Vazio 
                                                                           && (getPeca m (x+1) y == Bloco || getPeca m (x+1) y == Caixa) = Jogo (setPeca m Caixa (x+1) (y+1)) (Jogador (x,y) dir False)
@@ -70,25 +106,49 @@ interageCaixa jog@(Jogo m j@(Jogador (x,y) dir caixa)) | caixa && dir == Este &&
                                                        | otherwise = jog
                                                    
 
--- Devolve o tipo de peca duma dada posicao no mapa
+-- |Devolve o tipo de peca duma dada posicao no mapa.
+
+-- |Por exemplo:
+
+-- |getPeca   
+
+-- |getPeca 
 getPeca :: Mapa -> Int -> Int -> Peca
 getPeca [] _ _ = Vazio
 getPeca (h:t) x 0 = getLinha h x
 getPeca (h:t) x y = getPeca t x (y-1)
 
--- Axuliar para a getPeca que devolve a peca de uma linha de acordo com uma coordenada x
+-- Axuliar para a getPeca que devolve a peca de uma linha de acordo com uma coordenada x.
+
+-- |Por exemplo:
+
+-- |getLinha    
+
+-- |getLinha 
 getLinha :: [Peca] -> Int -> Peca
 getLinha [] _ = Vazio
 getLinha (h:t) 0 = h
 getLinha (h:t) x = getLinha t (x-1)
 
--- Trocar uma peça numa posição do mapa para o tipo recebido no argumento
+-- |Trocar uma peça numa posição do mapa para o tipo recebido no argumento.
+
+-- |Por exemplo:
+
+-- |setPeca    
+
+-- |setPeca 
 setPeca :: Mapa -> Peca -> Int -> Int -> Mapa 
 setPeca [] _ _ _ = []
 setPeca (h:t) p x 0 = setLinha h p x : t
 setPeca (h:t) p x y = h : setPeca t p x (y-1)
 
--- Auxiliar da SetPeca que altera a peça de uma certa linha para o tipo de peça recebido como argumento, mantendo igual o resto da linha
+-- |Auxiliar da SetPeca que altera a peça de uma certa linha para o tipo de peça recebido como argumento, mantendo igual o resto da linha.
+
+-- |Por exemplo:
+
+-- |setLinha    
+
+-- |setLinha 
 setLinha :: [Peca] -> Peca -> Int -> [Peca]
 setLinha [] _ _ = []
 setLinha (h:t) p 0 = p:t
