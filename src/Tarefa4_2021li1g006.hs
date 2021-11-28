15dd8641ceae56aefe2e6265e7ba6510710d0523
Mariana Pinto <a100756@alunos.uminho.pt>
2021-11-28 03:32:59 +0000

    interageCaixa

--- src/Tarefa4_2021li1g006.hs
+++ src/Tarefa4_2021li1g006.hs
@@ -16,7 +16,7 @@ moveJogador :: Jogo -> Movimento -> Jogo
 moveJogador (Jogo m j) AndarDireita = Jogo m (andaDirJogador j m)
 moveJogador (Jogo m j) AndarEsquerda = Jogo m (andaEsqJogador j m)
 moveJogador (Jogo m j) Trepar = Jogo m (trepa j m)
--- moveJogador (Jogo m j) InterageCaixa = 
+moveJogador j InterageCaixa = interageCaixa j
 
 
 correrMovimentos :: Jogo -> [Movimento] -> Jogo
@@ -52,15 +52,38 @@ andaEsqJogador (Jogador (x,y) esq caixa) m
              | getPeca m (x-1) y == Vazio =
                     if getPeca m (x-1) (y-1) == Bloco || getPeca m (x-1) (y-1) == Caixa then Jogador (x-1,y) Oeste caixa
                     else andaEsqJogador (Jogador (x,y-1) Oeste caixa) m
-                    
--- interageCaixa (fazer)
+
+interageCaixa :: Jogo -> Jogo  
+interageCaixa jog@(Jogo m j@(Jogador (x,y) dir caixa)) | caixa && dir == Este && getPeca m (x+1) (y+1) == Vazio 
+                                                                          && (getPeca m (x+1) y == Bloco || getPeca m (x+1) y == Caixa) = Jogo (setPeca m Caixa (x+1) (y+1)) (Jogador (x,y) dir False)
+                                                       | caixa && dir == Este && getPeca m (x+1) (y+1) == Vazio && getPeca m (x+1) y == Vazio = interageCaixa (Jogo m (Jogador (x,y-1) dir caixa))
+                                                       | caixa && dir == Oeste && getPeca m (x-1) (y+1) == Vazio 
+                                                                               && (getPeca m (x-1) y == Bloco || getPeca m (x-1) y == Caixa) = Jogo (setPeca m Caixa (x-1) (y+1)) (Jogador (x,y) dir False)
+                                                       | caixa && dir == Oeste && getPeca m (x-1) (y+1) == Vazio && getPeca m (x-1) y == Vazio = interageCaixa (Jogo m (Jogador (x,y-1) dir caixa))
+                                                       | not caixa && dir == Este && getPeca m (x+1) y == Caixa && getPeca m x (y+1) == Vazio && getPeca m (x+1) (y+1) == Vazio = Jogo (setPeca m Vazio (x+1) y) (Jogador (x,y) dir True)
+                                                       | not caixa && dir == Oeste && getPeca m (x-1) y == Caixa && getPeca m x (y+1) == Vazio && getPeca m (x-1) (y+1) == Vazio = Jogo (setPeca m Vazio (x-1) y) (Jogador (x,y) dir True)
+                                                       | otherwise = jog
+                                                   
 
 -- Devolve o tipo de peca duma dada posicao no mapa
 getPeca :: Mapa -> Int -> Int -> Peca
-getPeca [l] x 0 = getLinha l x
+getPeca [] _ _ = Vazio
+getPeca (h:t) x 0 = getLinha h x
 getPeca (h:t) x y = getPeca t x (y-1)
 
 -- Axuliar para a getPeca que devolve a peca de uma linha de acordo com uma coordenada x
 getLinha :: [Peca] -> Int -> Peca
-getLinha [p] 0 = p
+getLinha [] _ = Vazio
+getLinha (h:t) 0 = h
 getLinha (h:t) x = getLinha t (x-1)
+
+-- Trocar uma peça numa posição do mapa para o tipo recebido no argumento
+setPeca :: Mapa -> Peca -> Int -> Int -> Mapa 
+setPeca [] _ _ _ = []
+setPeca (h:t) p x 0 = setLinha h p x : t
+setPeca (h:t) p x y = h : setPeca t p x (y-1)
+
+setLinha :: [Peca] -> Peca -> Int -> [Peca]
+setLinha [] _ _ = []
+setLinha (h:t) p 0 = p:t
+setLinha (h:t) p x = h : setLinha t p (x-1)
