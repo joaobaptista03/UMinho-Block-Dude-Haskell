d2c80748c792fd6dd30708dc1502fd7800bc36ef
Tuga <Tuga>
2021-11-29 18:52:02 +0000

    Ficheiros para a Fase 2.

---
+++ src/Tarefa5_2021li1g006.hs
@@ -0,0 +1,15 @@
+{- |
+Module      : Tarefa5_2021li1g006
+Description : Aplicação gráfica completa
+Copyright   : João Pedro Mota Baptista <a100705@alunos.uminho.pt>;
+            : Mariana Pinto <a100756@alunos.uminho.pt>;
+
+Módulo para a realização da Tarefa 5 do projeto de LI1 em 2021/22.
+-}
+import Tarefa1_2021li1g006
+import Tarefa2_2021li1g006
+import Tarefa3_2021li1g006
+import Tarefa4_2021li1g006
+import LI12122
+
+module Tarefa5_2021li1g006 where
---
+++ src/Tarefa6_2021li1g006.hs
@@ -0,0 +1,16 @@
+{- |
+Module      : Tarefa6_2021li1g006
+Description : Resolução de um puzzle
+Copyright   : João Pedro Mota Baptista <a100705@alunos.uminho.pt>;
+            : Mariana Pinto <a100756@alunos.uminho.pt>;
+
+Módulo para a realização da Tarefa 6 do projeto de LI1 em 2021/22.
+-}
+import Tarefa1_2021li1g006
+import Tarefa2_2021li1g006
+import Tarefa3_2021li1g006
+import Tarefa4_2021li1g006
+import LI12122
+
+module Tarefa6_2021li1g006 where
+
