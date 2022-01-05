01b4cacb1cc20a3fffaeba11bad3ba2da5886279
a100705 <a100705@alunos.uminho.pt>
2022-01-05 21:26:45 +0000

    Nomes alterados

--- src/Tarefa5_2021li1g006.hs
+++ src/Tarefa5_2021li1g006.hs
@@ -29,17 +29,20 @@ data Menu = Controller Options
 window :: Display
 window = FullScreen
 
+background :: Color
+background = greyN 0.263
+
 fr :: Int
 fr = 50
 
-botoes :: String -> Picture 
-botoes texto = pictures
+makeButton :: String -> Picture 
+makeButton texto = pictures
     [ translate 0 50 $ color black $ rectangleSolid 204 54
     , translate 0 50 $ color (greyN 0.8) $ rectangleSolid 200 50
     , translate (-29) 38 $ scale 0.26 0.26 $ text texto]
 
-botaoinstructions :: Picture 
-botaoinstructions = pictures
+instructionsButton :: Picture 
+instructionsButton = pictures
     [ translate 0 (-150) $ color black $ rectangleSolid 204 54
     , translate 0 (-150) $ color (greyN 0.8) $ rectangleSolid 200 50
     , translate (-85) (-162) $ scale 0.26 0.26 $ text "Instructions"]
@@ -47,49 +50,49 @@ botaoinstructions = pictures
 
 
 draw :: Picture -> Picture -> Picture -> Picture -> Picture -> Picture -> Picture -> Picture -> Picture -> Menu -> Picture
-draw logo bloco caixa porta playerL playerR win mar tuga Win = Pictures [
+draw logo block box door playerL playerR win mar joaopedro Win = Pictures [
                                                                    Translate (-280) (-40) $ Color (dark green) $ translate 300 0 $ scale 2 2 win
                                                                  , translate (-740) 400 $ scale 1 1 logo
                                                                   ]
-draw logo bloco caixa porta playerL playerR win mar tuga (Controller Play) = Pictures [
-                                                                            Color (bright magenta) $ Translate 0 (-100) $ botoes "Play"
-                                                                          , botaoinstructions
-                                                                          , Translate 0 (-300) $ botoes "Exit"
+draw logo block box door playerL playerR win mar joaopedro (Controller Play) = Pictures [
+                                                                            Color (bright magenta) $ Translate 0 (-100) $ makeButton "Play"
+                                                                          , instructionsButton
+                                                                          , Translate 0 (-300) $ makeButton "Exit"
                                                                           , translate 0 300 $ scale 1 1 logo
                                                                           , color white $ translate (-350) (-520) $ scale 0.2 0.2 $ text "Copyright 2022 - Joao Pedro Baptista & Mariana Pinto"
                                                                           , translate 700 300 $ rectangleSolid 360 360
                                                                           , translate (-700) 300 $ rectangleSolid 360 360
-                                                                          , translate (-700) 300 tuga
+                                                                          , translate (-700) 300 joaopedro
                                                                           , translate 700 300 mar
                                                                            ]
-draw logo bloco caixa porta playerL playerR win mar tuga (Controller Instructions) = Pictures [
-                                                                            Translate 0 (-100) $ botoes "Play"
-                                                                          , Color (makeColor 0 0 256 1) botaoinstructions
-                                                                          , Translate 0 (-300) $ botoes "Exit"
+draw logo block box door playerL playerR win mar joaopedro (Controller Instructions) = Pictures [
+                                                                            Translate 0 (-100) $ makeButton "Play"
+                                                                          , Color (makeColor 0 0 256 1) instructionsButton
+                                                                          , Translate 0 (-300) $ makeButton "Exit"
                                                                           , translate 0 300 $ scale 1 1 logo
                                                                           , color white $ translate (-350) (-520) $ scale 0.2 0.2 $ text "Copyright 2022 - Joao Pedro Baptista & Mariana Pinto"
                                                                           , translate 700 300 $ rectangleSolid 360 360
                                                                           , translate (-700) 300 $ rectangleSolid 360 360
-                                                                          , translate (-700) 300 tuga
+                                                                          , translate (-700) 300 joaopedro
                                                                           , translate 700 300 mar
                                                                           ]
-draw logo bloco caixa porta playerL playerR win mar tuga (Controller Exit) = Pictures [
-                                                                           Translate 0 (-100) $ botoes "Play"
-                                                                         , botaoinstructions
-                                                                         , Color (makeColor 0 212 255 1) $ Translate 0 (-300) $ botoes "Exit"
+draw logo block box door playerL playerR win mar joaopedro (Controller Exit) = Pictures [
+                                                                           Translate 0 (-100) $ makeButton "Play"
+                                                                         , instructionsButton
+                                                                         , Color (makeColor 0 212 255 1) $ Translate 0 (-300) $ makeButton "Exit"
                                                                          , translate 0 300 $ scale 1 1 logo
                                                                          , color white $ translate (-350) (-520) $ scale 0.2 0.2 $ text "Copyright 2022 - Joao Pedro Baptista & Mariana Pinto"
                                                                          , translate 700 300 $ rectangleSolid 360 360
                                                                          , translate (-700) 300 $ rectangleSolid 360 360
-                                                                         , translate (-700) 300 tuga
+                                                                         , translate (-700) 300 joaopedro
                                                                          , translate 700 300 mar
                                                                           ]
-draw logo bloco caixa porta playerL playerR win mar tuga Instructionss = pictures [
+draw logo block box door playerL playerR win mar joaopedro Instructionss = pictures [
                                                                                     translate 0 300 $ scale 1 1 logo
                                                                                   , color white $ translate (-350) (-520) $ scale 0.2 0.2 $ text "Copyright 2022 - Joao Pedro Baptista & Mariana Pinto"
                                                                                   , translate 700 300 $ rectangleSolid 360 360
                                                                                   , translate (-700) 300 $ rectangleSolid 360 360
-                                                                                  , translate (-700) 300 tuga
+                                                                                  , translate (-700) 300 joaopedro
                                                                                   , translate 700 300 mar
                                                                                   , color white $ translate (-230) (-35) $ scale 0.2 0.2 $ text "Press ' r ' to restart"
                                                                                   , color white $ translate (-230) (-70) $ scale 0.2 0.2 $ text "Press ' m ' to go back to menu"
@@ -98,11 +101,11 @@ draw logo bloco caixa porta playerL playerR win mar tuga Instructionss = picture
                                                                                   , color white $ translate (-230) (-175) $ scale 0.2 0.2 $ text "Press ' s ' or keyDown to carry a box"
                                                                                   , color white $ translate (-230) (-210) $ scale 0.2 0.2 $ text "Press ' d ' or keyRight to walk right"
                                                                                   ]
-draw logo bloco caixa porta playerL playerR win mar tuga (GameMode (Jogo m (Jogador (x,y) d c))) = pictures [
-                                                                                                translate (-550) 130 $ paraGloss bloco caixa porta (desconstroiMapa m)
+draw logo block box door playerL playerR win mar joaopedro (GameMode (Jogo m (Jogador (x,y) d c))) = pictures [
+                                                                                                translate (-550) 130 $ paraGloss block box door (desconstroiMapa m)
                                                                                               , translate (-550) 130 $ Pictures [
                                                                                                                                 if d == Oeste then Translate (64 * fromIntegral x) ((-64) * fromIntegral y) $ scale 2 2 playerL else Translate (64 * fromIntegral x) ((-64) * fromIntegral y) $ scale 2 2 playerR
-                                                                                                                              , if c then translate (64 * fromIntegral x) ((-64) * fromIntegral (y-1)) $ scale 2 2 caixa else Blank
+                                                                                                                              , if c then translate (64 * fromIntegral x) ((-64) * fromIntegral (y-1)) $ scale 2 2 box else Blank
                                                                                                                                 ]
                                                                                               , color white $ translate 150 385 $ scale 0.2 0.2 $ text "Copyright 2022 - Joao Pedro Baptista & Mariana Pinto"
                                                                                               , translate (-740) 400 $ scale 1 1 logo
@@ -110,10 +113,10 @@ draw logo bloco caixa porta playerL playerR win mar tuga (GameMode (Jogo m (Joga
 
 paraGloss :: Picture -> Picture -> Picture -> [(Peca, Coordenadas)] -> Picture
 paraGloss _ _ _ [] = Blank
-paraGloss bloco caixa porta ((p, (x,y)):t) = pictures 
+paraGloss block box door ((p, (x,y)):t) = pictures 
                                                       [
-                                                        translate i j $ scale 2 2 (if p == Bloco then bloco else if p == Caixa then caixa else porta)
-                                                      , paraGloss bloco caixa porta t
+                                                        translate i j $ scale 2 2 (if p == Bloco then block else if p == Caixa then box else door)
+                                                      , paraGloss block box door t
                                                       ]
     where
       i = 64 * fromIntegral x
@@ -135,10 +138,7 @@ mapa1 =         [
                 , [Bloco, Vazio, Vazio, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Vazio, Vazio, Vazio, Vazio, Bloco, Vazio, Vazio, Bloco]
                 , [Bloco, Vazio, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Vazio, Vazio, Vazio, Bloco, Vazio, Vazio, Porta]
                 , [Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco]
-                                                                                                                          ]
-
-time :: Float -> Menu -> Menu
-time _ s = s
+                                                                                                                                               ]
 
 event :: Event -> Menu -> Menu
 event (EventKey (SpecialKey KeyEnter) Down _ _) (Controller Play) = GameMode jogoinicial
@@ -167,15 +167,27 @@ event _ w = w
 estado :: Menu
 estado = Controller Play
 
+time :: Float -> Menu -> Menu
+time _ s = s
+
 main :: IO ()
 main = do
-  tuga <- loadBMP "imgs/Tuga.bmp"
-  mariana <- loadBMP "imgs/Mariana.bmp"
-  win <- loadBMP "imgs/YouWin.bmp"
-  playerL <- loadBMP "imgs/playerL.bmp"
-  playerR <- loadBMP "imgs/playerR.bmp"
-  caixa <- loadBMP "imgs/Caixa.bmp"
-  logo <- loadBMP "imgs/Logo.bmp"
-  bloco <- loadBMP "imgs/Bloco.bmp"
-  porta <- loadBMP "imgs/Porta.bmp"
-  play window (greyN 0.263) fr estado (draw logo bloco caixa porta playerL playerR win mariana tuga) event time
+
+  joaopedro <- loadBMP "imgs/joaopedro.bmp"
+  mariana <- loadBMP "imgs/mariana.bmp"
+  win <- loadBMP "imgs/youwin.bmp"
+  playerL <- loadBMP "imgs/playerl.bmp"
+  playerR <- loadBMP "imgs/playerr.bmp"
+  box <- loadBMP "imgs/box.bmp"
+  logo <- loadBMP "imgs/logo.bmp"
+  block <- loadBMP "imgs/block.bmp"
+  door <- loadBMP "imgs/door.bmp"
+
+  play 
+    FullScreen
+    background
+    fr
+    estado
+    (draw logo block box door playerL playerR win mariana joaopedro)
+    event
+    time
