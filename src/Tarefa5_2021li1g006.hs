b83ebb1f73af385c3f14ca73d2728f036df32635
a100705 <a100705@alunos.uminho.pt>
2022-01-06 02:08:11 +0000

    Documentação da T5.

--- src/Tarefa5_2021li1g006.hs
+++ src/Tarefa5_2021li1g006.hs
@@ -17,44 +17,49 @@ import Tarefa3_2021li1g006
 import Tarefa4_2021li1g006
 import LI12122
 
+-- | Opções do mnu: Play, Instructions e Exit.
 data Options = Play
             |  Instructions
             |  Exit
 
-data Menu = Controller Options
-          | GameMode Jogo
-          | Instructionss
-          | Win
+-- | Todos os estados possíveis: Controller seguido do data Options (Menu e a opção selecionada), Gamemode seguido do Jogo (Modo de Jogo), Instructionss (Página das instruções), Win (Quando o player ganha)
+data Status = Controller Options
+            | GameMode Jogo
+            | Instructionss
+            | Win
 
--- | Dimensão da janela do jogo.
-
+-- | Janela do jogo, que neste caso é Fullscreen.
 window :: Display
 window = FullScreen
 
 -- | Cor de fundo.
-
 background :: Color
 background = greyN 0.263
 
 -- | Frames por segundo.
-
 fr :: Int
-fr = 50
+fr = 30
 
+-- | Esta função cria um botão consoante a string que lhe é dada para criar as opções do menu.
+
+-- | Por exemplo:
+
+-- | makeButton Play produz a picture do botão play do menu.
 makeButton :: String -> Picture 
 makeButton texto = pictures
     [ translate 0 50 $ color black $ rectangleSolid 204 54
     , translate 0 50 $ color (greyN 0.8) $ rectangleSolid 200 50
     , translate (-29) 38 $ scale 0.26 0.26 $ text texto]
 
+-- | instructionsButton é especificamente o botão do menu de Instrutcions. Não dá para usar a função makeButton pois "Instructions" não ficava centrado no botão, tendo de criá-lo à parte.
 instructionsButton :: Picture 
 instructionsButton = pictures
     [ translate 0 (-150) $ color black $ rectangleSolid 204 54
     , translate 0 (-150) $ color (greyN 0.8) $ rectangleSolid 200 50
     , translate (-85) (-162) $ scale 0.26 0.26 $ text "Instructions"]
 
-
-draw :: Picture -> Picture -> Picture -> Picture -> Picture -> Picture -> Picture -> Picture -> Picture -> Menu -> Picture
+-- | draw é a função que desenha tudo na janela: Menu quando as várias opções estão selecionadas, Modo de Jogo, Estado de Venceu e Página de Instruções.
+draw :: Picture -> Picture -> Picture -> Picture -> Picture -> Picture -> Picture -> Picture -> Picture -> Status -> Picture
 draw logo block box door playerL playerR win mar joaopedro Win = Pictures [
                                                                    Translate (-280) (-40) $ Color (dark green) $ translate 300 0 $ scale 2 2 win
                                                                  , translate (-740) 400 $ scale 1 1 logo
@@ -116,6 +121,7 @@ draw logo block box door playerL playerR win mar joaopedro (GameMode (Jogo m (Jo
                                                                                               , translate (-740) 400 $ scale 1 1 logo
                                                                                                ]
 
+-- | paraGloss traduz as coordenadas da fase 1 para as coordenadas do Gloss e também desenha as peças do jogo, sendo uma função auxiliar da função draw, quando estamos no Modo de Jogo.
 paraGloss :: Picture -> Picture -> Picture -> [(Peca, Coordenadas)] -> Picture
 paraGloss _ _ _ [] = Blank
 paraGloss block box door ((p, (x,y)):t) = pictures 
@@ -128,12 +134,16 @@ paraGloss block box door ((p, (x,y)):t) = pictures
       j = (-64) * fromIntegral y
 
 -- | Ponto de partida do jogo, que inclui o mapa1.
-
 jogoinicial :: Jogo 
 jogoinicial = Jogo mapa1 (Jogador (1,9) Este False)
 
--- | Primeiro mapa do jogo.
+-- | A função termina o jogo quando o player chega à porta.
+finishgame :: Float -> Status -> Status
+finishgame _ (GameMode (Jogo m (Jogador (x,y) d c))) | snd (saberPorta (desconstroiMapa m)) == (x,y) = Win
+                                                     | otherwise = GameMode (Jogo m (Jogador (x,y) d c))
+finishgame _ o = o
 
+-- | Primeiro mapa do jogo.
 mapa1 :: Mapa
 mapa1 =         [
                   [Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco]
@@ -149,13 +159,12 @@ mapa1 =         [
                 , [Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco]
                                                                                                                                               ]
 
--- | A função event transforma cada de input do Jogador na sua respetiva ação/movimento. 
+-- | A função event transforma cada de input do Jogador na sua respetiva ação/movimento.
 
 -- |Por exemplo:
 
 -- | Se o jogador clicar no "a" ou no "d", anda, respetivamente, para a esquerda ou direita.
-
-event :: Event -> Menu -> Menu
+event :: Event -> Status -> Status
 event (EventKey (SpecialKey KeyEnter) Down _ _) (Controller Play) = GameMode jogoinicial
 event (EventKey (Char 'r') Down _ _) (GameMode _) = GameMode jogoinicial
 event (EventKey (Char 'm') Down _ _) _ = Controller Play
@@ -179,12 +188,11 @@ event (EventKey (Char 'a') Down _ _) (GameMode (Jogo m (Jogador (x,y) d c))) = G
 event (EventKey (Char 'd') Down _ _) (GameMode (Jogo m (Jogador (x,y) d c))) = GameMode (Jogo m (andaDirJogador (Jogador (x,y) d c) m))
 event _ w = w
 
-estado :: Menu
+-- | Estado inicial do programa quando ele é aberto, que neste caso é o menu com o botão Play selecionado.
+estado :: Status
 estado = Controller Play
 
-time :: Float -> Menu -> Menu
-time _ s = s
-
+-- | Função principal que junta todas as funções atrás mencionadas, carregando todas as imagens para produzir a janela com o jogo desenhado, sendo ele atualizado consoante os inputs que o utilizador dá.
 main :: IO ()
 main = do
 
@@ -205,14 +213,4 @@ main = do
     estado
     (draw logo block box door playerL playerR win mariana joaopedro)
     event
-    time
-
-
--- |A função termina o jogo quando o player chega à porta.
-
-finishgame :: Float -> Menu -> Menu
-finishgame _ (GameMode (Jogo m (Jogador (x,y) d c))) | snd (saberPorta (desconstroiMapa m)) == (x,y) = Win
-                                                     | otherwise = GameMode (Jogo m (Jogador (x,y) d c))
-finishgame _ o = o
-
-
+    finishgame
