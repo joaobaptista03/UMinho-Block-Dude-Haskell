adfd8380094d0be4976c91a3a30e0defd0b33d7a
a100705 <a100705@alunos.uminho.pt>
2022-01-08 20:57:47 +0000

    Função final e documentação da T6 Feita.

--- src/Tarefa6_2021li1g006.hs
+++ src/Tarefa6_2021li1g006.hs
@@ -18,10 +18,22 @@ import Data.Maybe
 
 import LI12122
 
+-- | Função final que, dando o número de movimentos máximos e um Jogo, devolve, se possível os movimentos que resolvem o puzzle, caso contrário retorna Nothing.
 
+-- | Por exemplo:
+
+-- | resolveJogo 16 (Jogo mapa1 (Jogador (1,9) Este False)) == Just [Trepar,Trepar,Trepar,Trepar,Trepar,AndarDireita,AndarDireita,AndarDireita,AndarDireita,AndarDireita,AndarDireita,AndarDireita,Trepar,AndarDireita,AndarDireita,AndarDireita]
+
+-- | resolveJogo 15 (Jogo mapa1 (Jogador (1,9) Este False)) == Nothing
+resolveJogo :: Int -> Jogo -> Maybe [Movimento]
+resolveJogo a (Jogo m (Jogador (x,y) Este c)) | snd (auxauxRS (Jogo m (Jogador (x,y) Este c))) <= a = Just (map fromJust (fst (auxauxRS (Jogo m (Jogador (x,y) Este c)))))
+                                              | otherwise = Nothing
+
+-- | Função auxiliar da auxRS que, dando o jogo, calcula os movimentos mínimos para resolver o puzzle e retorna um tuplo com esses movimentos em Just, seguido do número de movimentos.
 auxauxRS :: Jogo -> ([Maybe Movimento],Int)
 auxauxRS (Jogo m (Jogador (x,y) Este c)) = (auxRS (Jogo m (Jogador (x,y) Este c)), length (auxRS (Jogo m (Jogador (x,y) Este c))))
 
+-- | Função auxiliar da principal que, dado o jogo, calcula os movimentos mínimos para resolver o puzzle e retorna esses movimentos em Just.
 auxRS :: Jogo -> [Maybe Movimento]
 auxRS (Jogo m (Jogador (x,y) Este False)) | snd (saberPorta (desconstroiMapa m)) == (x,y) = []
 auxRS (Jogo m (Jogador (x,y) Este False)) | getPeca m (x+1) y == Vazio && (getPeca m (x+1) (y+1) == Bloco || getPeca m (x+1) (y+1) == Caixa) = Just AndarDireita : auxRS (Jogo m (andaDirJogador (Jogador (x,y) Este False) m))
