e88a09682a1a269d7684a1a703da9db6f6c7e279
Mariana Pinto <a100756@alunos.uminho.pt>
2022-01-08 20:48:24 +0000

    adicionada função auxauxRS t6

--- src/Tarefa6_2021li1g006.hs
+++ src/Tarefa6_2021li1g006.hs
@@ -6,11 +6,26 @@ Copyright   : João Pedro Mota Baptista <a100705@alunos.uminho.pt>;
 
 Módulo para a realização da Tarefa 6 do projeto de LI1 em 2021/22.
 -}
+
+module Tarefa6_2021li1g006 where
+
 import Tarefa1_2021li1g006
 import Tarefa2_2021li1g006
 import Tarefa3_2021li1g006
 import Tarefa4_2021li1g006
+import Tarefa5_2021li1g006
+import Data.Maybe
+
 import LI12122
+import qualified Data.Functor as Nothing
 
-module Tarefa6_2021li1g006 where
 
+auxauxRS :: Jogo -> ([Maybe Movimento],Int)
+auxauxRS (Jogo m (Jogador (x,y) Este c)) = (auxRS (Jogo m (Jogador (x,y) Este c)), length (auxRS (Jogo m (Jogador (x,y) Este c))))
+
+auxRS :: Jogo -> [Maybe Movimento]
+auxRS (Jogo m (Jogador (x,y) Este False)) | snd (saberPorta (desconstroiMapa m)) == (x,y) = []
+auxRS (Jogo m (Jogador (x,y) Este False)) | getPeca m (x+1) y == Vazio && (getPeca m (x+1) (y+1) == Bloco || getPeca m (x+1) (y+1) == Caixa) = Just AndarDireita : auxRS (Jogo m (andaDirJogador (Jogador (x,y) Este False) m))
+                                          | getPeca m (x+1) y == Vazio && getPeca m (x+1) (y+1) == Vazio && (getPeca m (x+1) (y+2) == Bloco || getPeca m (x+1) (y+2) == Caixa) = Just AndarDireita : auxRS (Jogo m (andaDirJogador (Jogador (x,y) Este False) m))
+                                          | (getPeca m (x+1) y == Bloco || getPeca m (x+1) y == Caixa) && getPeca m (x+1) (y-1) == Vazio = Just Trepar : auxRS (Jogo m (trepa (Jogador (x,y) Este False) m))
+                                          | getPeca m (x+1) y == Porta = Just AndarDireita : auxRS (Jogo m (andaDirJogador (Jogador (x,y) Este False) m))
